a4b22f6b0b5698eed2a489a7596cae0b450e149e
Lucas Magotteaux <lucas.magotteaux@hotmail.com>
2023-04-03 16:49:45 +0200

    Delete airslide directory

--- airslide/login.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-osascript -e 'tell application "System Events" to make login item at end with properties {path:"/Applications/Airslide.app", hidden:false}'
--- airslide/nologin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-osascript -e 'tell application "System Events" to delete login item "Airslide"'
-
-
